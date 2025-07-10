8d634169da1557593640c15a51b0a738e7e7db3a
0xDev05 <divpatel1274@gmail.com>
2025-07-10 23:51:24 +0530

    Payment done

--- anchor-vault/programs/anchor-vault/src/lib.rs
+++ anchor-vault/programs/anchor-vault/src/lib.rs
@@ -1,6 +1,6 @@
 #![allow(unexpected_cfgs)]
 #![allow(deprecated)]
-use anchor_lang::{prelude::*, system_program::{transfer, Transfer}};
+use anchor_lang::{accounts::signer, prelude::*, system_program::{transfer, Transfer}};
 
 declare_id!("26p9wRdb4s1gV9TnkzmoTHtR5PfEqWkT37YongR6gk9m");
 
@@ -55,6 +55,64 @@ impl<'info> Initialize<'info> {
         Ok(())
     }
 }
+
+
+#[derive(Accounts)]
+pub struct Payment<'info>{
+    #[account(mut)]
+    pub user:Signer<'info>,
+    #[account(
+        init,
+        payer=user,
+        seeds=[b"state",user.key.as_ref()],
+        bumps = vault_state.vault_bump,
+        space = Vault_state::INIT_SPACE
+    )]
+    pub vault_state: Account<'info , Vault_state>;
+    #[account(
+        mut,
+        seeds = [b"vault",vault_state.key.as_ref()],
+        bumps:vault_state.vault_bump
+    )]
+    pub vault : SystemAccount<'info>,
+    pub system_program: Program<'info,System>
+}
+
+
+impl<'info> Payment<'info>{
+    pub fn deposit(&mut self , amount: u64) -> Result<()> {
+
+        let cpi_context = CpiContext::new(
+            self.system_program.to_account_info(),
+            Transfer{
+                from:self.user.to_account_info(),
+                to:self.vault.to_account_info()
+            }
+        );
+
+        transfer(cpi_context, amount)?;
+        Ok(())
+    }
+
+    pub fn withdraw(&mut self , amount: u64) -> Result<()>  {
+
+        let signer_seeds = &[
+            b"vault",
+            self.vault_state.to_account_info().key.as_ref(),
+            &[self.vault_state.vault_bump]
+        ];
+
+        let cpi_context = CpiContext::new_with_signer(self.system_program.to_account_info(), Transfer{
+            from:self.vault.to_account_info(),
+            to:self.user.to_account_info()
+        },& [signer_seeds]);
+
+        Ok(())
+    }
+}
+
+
+
 
 
 #[account]
